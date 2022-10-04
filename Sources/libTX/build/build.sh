0cf0da85f00b0c1b5753171a17f0a84127fd7c20
Alexander Cyon <alex.cyon@rdx.works>
2022-10-04 17:51:56 +0200

    reverting manual creation of xcframework, since turns out not needed, we can rely on 'xcodebuild -create-xcframework', the warnings was cause by change of header, were C method now use 'const char *' instead of 'char *' so just needed to update code.

--- Sources/libTX/build/build.sh
+++ Sources/libTX/build/build.sh
@@ -34,6 +34,9 @@ echo "🔮 🛠 🎯 Finished building all targets ✅"
         aarch64-apple-darwin/release/libtransaction_library.a \
         x86_64-apple-darwin/release/libtransaction_library.a \
         -o macos-arm64_x86_64/libTX.a
+        
+    mv aarch64-apple-ios/release/libtransaction_library.a aarch64-apple-ios/release/libTX.a
+    mv aarch64-apple-ios-sim/release/libtransaction_library.a aarch64-apple-ios-sim/release/libTX.a
 
 	echo "🔮 🙏 Finished merging some of the targets using 'lipo'"
 )
@@ -47,50 +50,47 @@ echo "🔮 🛠 🎯 Finished building all targets ✅"
     rustup default nightly
 
     # Creating the header file
-	echo "🔮 Creating header file..."
+	echo "🔮 👤 Creating header file..."
     cbindgen \
         --lang c \
         --config cbindgen.toml \
         --crate transaction-library \
         --output libTX.h
+        
+    echo "🔮 Making sure folder 'include' exists"
+    mkdir -p include
+
+    echo "🔮 👤 Copying header 'libTX.h' to 'include' folder"
+    cp libTX.h include
+
+    echo "🔮 🗺 Copying header 'module.modulemap' to 'include' folder"
+    cp ../module.modulemap include
+
+    echo "🔮 🗂 Copying 'include' folder for iOS target"
+    cp -r include target/aarch64-apple-ios/release/
+
+    echo "🔮 🗂 Copying 'include' folder for iOS Sim target"
+    cp -r include target/aarch64-apple-ios-sim/release/
+
+    echo "🔮 🗂 Copying 'include' folder for Mac target"
+    cp -r include target/macos-arm64_x86_64/
+
+    echo "🔮 🗑 Removing folder 'include'"
+    rm -r include
 
     echo "🔮 🦀 Restore rust to stable ⚖️"
     rustup default stable
 )
 
-echo "🔮 📦 Manually packaging '.xcframework' ☑️"
+echo "🔮 📦 Creating '.xcframework' ☑️"
 
-# `xcodebuild -create-xcframework` does not work for us, or at least we have not yet found
-# the proper command. We have been using `xcodebuild -create-xcframework` to create the
-# `Info.plist` file originally, with the three values in the `AvailableLibraries`.
-# The `HeaderPath` value inside each element in `AvailableLibraries` has originally been
-# using a seperate Header, this results in symbol conflicts when we try to build the
-# TransactionKit SPM target, error: "type of expression is ambiguous without more context"
-# This is using Xcode Version 14.0.1 (14A400) on Apple Silicon MacOS 12.6 (21G115).
+xcodebuild -create-xcframework \
+    -library target/aarch64-apple-ios/release/libTX.a \
+    -headers target/aarch64-apple-ios/release/include \
+    -library target/aarch64-apple-ios-sim/release/libTX.a \
+    -headers target/aarch64-apple-ios-sim/release/include \
+    -library target/macos-arm64_x86_64/libTX.a \
+    -headers target/macos-arm64_x86_64/include \
+    -output ../../libTX.xcframework
 
-cd $script_parent
-cd ..
-
-rm -rf libTX.xcframework
-mkdir libTX.xcframework
-mkdir libTX.xcframework/Headers
-mkdir libTX.xcframework/ios-arm64
-mkdir libTX.xcframework/ios-arm64-simulator
-mkdir libTX.xcframework/macos-arm64_x86_64
-cp build/Info.plist libTX.xcframework/
-cp build/module.modulemap libTX.xcframework/Headers/
-cp build/transaction-library/libTX.h libTX.xcframework/Headers/
-cp build/transaction-library/target/macos-arm64_x86_64/libTX.a libTX.xcframework/macos-arm64_x86_64/
-cp build/transaction-library/target/aarch64-apple-ios/release/libtransaction_library.a libTX.xcframework/ios-arm64/libTX.a
-cp build/transaction-library/target/aarch64-apple-ios-sim/release/libtransaction_library.a libTX.xcframework/ios-arm64-simulator/libTX.a
-
-#xcodebuild -create-xcframework \
-#    -library target/aarch64-apple-ios/release/libTX.a \
-#    -headers target/aarch64-apple-ios/release/include \
-#    -library target/aarch64-apple-ios-sim/release/libTX.a \
-#    -headers target/aarch64-apple-ios-sim/release/include \
-#    -library target/macos-arm64_x86_64/libTX.a \
-#    -headers target/macos-arm64_x86_64/include \
-#    -output ../../libTX.xcframework
-
-echo "🔮 Done! ✅"
+echo "🔮 📦 Created '.xcframework' ✅"
