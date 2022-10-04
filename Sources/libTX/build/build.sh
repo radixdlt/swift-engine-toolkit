1c7f266481be9cb33361f81556974bf357ce9431
Alexander Cyon <alex.cyon@rdx.works>
2022-10-04 14:25:00 +0200

    add git submodule

--- Sources/libTX/build/build.sh
+++ Sources/libTX/build/build.sh
@@ -1,5 +1,7 @@
 echo "Building the library";
 
+cd ./transaction-library
+
 # cargo build --target x86_64-apple-ios --release         # iOS Simulator & iPhone x86 target
 cargo build --target aarch64-apple-ios-sim --release    # iOS Simulator Aarch64 target
 cargo build --target aarch64-apple-ios --release        # iOS iPhone Aarch64 target
@@ -7,16 +9,20 @@ cargo build --target aarch64-apple-ios --release        # iOS iPhone Aarch64 tar
 cargo build --target aarch64-apple-darwin --release # Apple Silicon Mac
 cargo build --target x86_64-apple-darwin --release # Intel Mac
 
+# Create a fat binary for mac (combining Intel Mac + Apple Silicon Mac)
 (
     cd ./target
-
-    mkdir macOS
+    mkdir macos-arm64_x86_64
 
      # Combine the two macOS builds into one fat file
     lipo -create \
         aarch64-apple-darwin/release/libtransaction_library.a \
         x86_64-apple-darwin/release/libtransaction_library.a \
-        -o macOS/libtransaction_library.a
+        -o macos-arm64_x86_64/libTX.a
+        
+    # Rename iOS `.a`-files.
+    mv aarch64-apple-ios/release/libtransaction_library.a aarch64-apple-ios/release/libTX.a
+    mv aarch64-apple-ios-sim/release/libtransaction_library.a aarch64-apple-ios-sim/release/libTX.a
 )
 
 # Create the C header of the provided functions and adding it to the directory of each of the 
@@ -26,18 +32,12 @@ cargo build --target x86_64-apple-darwin --release # Intel Mac
 
     # Creating the header file
     cbindgen \
+        --lang c \
         --config cbindgen.toml \
         --crate transaction-library \
-        --output transaction_library.h
-
-    # Copying the header file to all of the builds
-    cp transaction_library.h ./target/aarch64-apple-ios-sim/release/
-    cp transaction_library.h ./target/aarch64-apple-ios/release/
-    
-    cp transaction_library.h ./target/macOS
-    
-    # The root-level header is no longer needed now. Safe to delete
-    rm transaction_library.h
+        --output libTX.h
+        
+    cp libTX.h ./target/libTX.h
 
     rustup default stable
 )
@@ -46,12 +46,12 @@ cargo build --target x86_64-apple-darwin --release # Intel Mac
 # removed the `x86_64-apple-ios` target from the XC Framework as it clashed with 
 # `ios-arm64-simulator`. If this causes issues, then we can look into it further. 
 xcodebuild -create-xcframework \
-    -library ./target/aarch64-apple-ios/release/libtransaction_library.a \
-    -headers ./target/aarch64-apple-ios/release/transaction_library.h \
-    -library ./target/aarch64-apple-ios-sim/release/libtransaction_library.a \
-    -headers ./target/aarch64-apple-ios-sim/release/transaction_library.h \
-    -library ./target/macOS/libtransaction_library.a \
-    -headers ./target/macOS/transaction_library.h \
-    -output ./target/TX_lib.xcframework
+    -library ./target/aarch64-apple-ios/release/libTX.a \
+    -library ./target/aarch64-apple-ios-sim/release/libTX.a \
+    -library ./target/macos-arm64_x86_64/libTX.a \
+    -headers ./target/libTX.h \
+    -output ./target/libTX.xcframework
+
+touch
 
 echo "Done!"
