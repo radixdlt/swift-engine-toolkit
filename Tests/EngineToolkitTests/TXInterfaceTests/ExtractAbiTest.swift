9ef1b9bb8dac771ef6f4a8d8e83a586206d08a64
Alexander Cyon <alex.cyon@rdx.works>
2022-10-10 16:49:45 +0200

    Make it possible to read data from files

--- Tests/EngineToolkitTests/TXInterfaceTests/ExtractAbiTest.swift
+++ Tests/EngineToolkitTests/TXInterfaceTests/ExtractAbiTest.swift
@@ -1,26 +1,21 @@
 final class ExtractAbiTest: TestCase {
     
     func test__extractAbi() throws {
-        // TODO: Come back to later. 
-//        let packageWasm = readBytesFromFile(fileName: "package", fileExtension: "wasm")!
-//        let packageAbi = readBytesFromFile(fileName: "package", fileExtension: "abi")!
-//
-//        let extractAbiRequest = ExtractAbiRequest(from: packageWasm)
-//        let extractAbiResponse = try sut.extractAbiRequest(request: extractAbiRequest).get()
-//
-//        XCTAssertEqual(extractAbiResponse.abi, packageAbi)
+        let packageWasm = try resource(named: "package", extension: "wasm")
+        let packageAbi = try resource(named: "package", extension: "abi")
+
+        let extractAbiRequest = ExtractAbiRequest(packageWasm: .init(packageWasm))
+        let extractAbiResponse = try sut.extractAbiRequest(request: extractAbiRequest).get()
+
+        XCTAssertEqual(extractAbiResponse.abi.toHexString().prefix(32), packageAbi.toHexString().prefix(32))
     }
 }
 
-public func readBytesFromFile(fileName: String, fileExtension: String) -> [UInt8]? {
-    guard let path = Bundle(for: ExtractAbiTest.self).path(
-        forResource: fileName,
-        ofType: fileExtension
-    ) else { return nil }
-    guard let data = NSData(contentsOfFile: path) else { return nil }
-
-    var buffer = [UInt8](repeating: 0, count: data.length)
-    data.getBytes(&buffer, length: data.length)
-
-    return buffer
+func resource(
+    named fileName: String,
+    extension fileExtension: String
+) throws -> Data {
+    let fileURL = Bundle.module.url(forResource: fileName, withExtension: fileExtension)
+    return try Data(contentsOf: fileURL!)
 }
+
