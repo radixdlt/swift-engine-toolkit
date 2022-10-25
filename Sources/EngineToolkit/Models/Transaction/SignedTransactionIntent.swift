36b6218c34d8cd2fda301c9a9cb183bf5d2c83ce
Alexander Cyon <alex.cyon@rdx.works>
2022-10-25 14:36:52 +0200

    Make Transaction initializers public

--- Sources/EngineToolkit/Models/Transaction/SignedTransactionIntent.swift
+++ Sources/EngineToolkit/Models/Transaction/SignedTransactionIntent.swift
@@ -11,6 +11,14 @@ public struct SignedTransactionIntent: Sendable, Codable, Hashable {
     public let intent: TransactionIntent
     public let intentSignatures: [Engine.SignatureWithPublicKey]
     
+    public init(
+        intent: TransactionIntent,
+        intentSignatures: [Engine.SignatureWithPublicKey]
+    ) {
+        self.intent = intent
+        self.intentSignatures = intentSignatures
+    }
+    
     private enum CodingKeys: String, CodingKey {
         case intent = "intent"
         case intentSignatures = "intent_signatures"
