12a62e5ca0ea99eca9ce3490559b43a0e02e7e21
Alexander Cyon <alex.cyon@gmail.com>
2022-10-31 07:49:36 +0100

    Trying to fix SPM issue 5630

--- Package.swift
+++ Package.swift
@@ -16,7 +16,14 @@ let package = Package(
         
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", from: "1.0.1"),
         
-        .package(url: "git@github.com:radixdlt/SLIP10.git", from: "0.0.17"),
+		// Due to SPM bug We MUST explicitly declare dependency on all packages
+		// that SLIP10 depend on
+		// https://forums.swift.org/t/package-built-with-dependency-on-another-package-that-uses-a-build-tool-plugin-fails-to-find-the-build-tool-product/58408/2
+		// This is very unfortunate and we hope it gets fixed in the future.
+		.package(url: "https://github.com/Sajjon/K1.git", from: "0.0.4"),
+		.package(url: "git@github.com:radixdlt/Bite.git", from: "0.0.1"),
+		.package(url: "git@github.com:radixdlt/Mnemonic.git", from: "0.0.4"),
+		.package(url: "git@github.com:radixdlt/SLIP10.git", from: "0.0.17"),
         
         // Haskell-like `newtype` feature.
         .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.7.0"),
@@ -30,7 +37,16 @@ let package = Package(
             name: "EngineToolkit",
             dependencies: [
                 "RadixEngineToolkit",
+				
+				// Due to SPM bug We MUST explicitly declare dependency on all packages
+				// that SLIP10 depend on
+				// https://forums.swift.org/t/package-built-with-dependency-on-another-package-that-uses-a-build-tool-plugin-fails-to-find-the-build-tool-product/58408/2
+				// This is very unfortunate and we hope it gets fixed in the future.
+				.product(name: "Bite", package: "Bite"),
+				.product(name: "K1", package: "K1"),
+				.product(name: "Mnemonic", package: "Mnemonic"),
 				.product(name: "SLIP10", package: "SLIP10"),
+				
                 .product(name: "Tagged", package: "swift-tagged"),
             ]
         ),
