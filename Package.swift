c627f534631cb6a1551b0dfd0dd62615634a5f32
Ghenadie Vasiliev-Pusca <ghenadie.vasiliev-pusca@rdx.works>
2023-07-03 17:11:46 +0300

    clean up repo

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,20 +13,7 @@ let package = Package(
             targets: ["EngineToolkit"]),
     ],
     dependencies: [
-        
         .package(url: "https://github.com/krzysztofzablocki/Difference.git", from: "1.0.1"),
-        
-		// Due to SPM bug We MUST explicitly declare dependency on all packages
-		// that SLIP10 depend on
-		// https://forums.swift.org/t/package-built-with-dependency-on-another-package-that-uses-a-build-tool-plugin-fails-to-find-the-build-tool-product/58408/2
-		// This is very unfortunate and we hope it gets fixed in the future.
-		.package(url: "https://github.com/Sajjon/K1.git", from: "0.0.4"),
-		.package(url: "git@github.com:radixdlt/Bite.git", from: "0.0.1"),
-		.package(url: "git@github.com:radixdlt/Mnemonic.git", from: "0.0.5"),
-		.package(url: "git@github.com:radixdlt/SLIP10.git", from: "0.0.19"),
-        
-        // Haskell-like `newtype` feature.
-        .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.7.0"),
     ],
     targets: [
         .binaryTarget(
@@ -37,17 +24,6 @@ let package = Package(
             name: "EngineToolkit",
             dependencies: [
                 "RadixEngineToolkit",
-				
-				// Due to SPM bug We MUST explicitly declare dependency on all packages
-				// that SLIP10 depend on
-				// https://forums.swift.org/t/package-built-with-dependency-on-another-package-that-uses-a-build-tool-plugin-fails-to-find-the-build-tool-product/58408/2
-				// This is very unfortunate and we hope it gets fixed in the future.
-				.product(name: "Bite", package: "Bite"),
-				.product(name: "K1", package: "K1"),
-				.product(name: "Mnemonic", package: "Mnemonic"),
-				.product(name: "SLIP10", package: "SLIP10"),
-				
-                .product(name: "Tagged", package: "swift-tagged"),
             ]
         ),
         .testTarget(
@@ -56,9 +32,7 @@ let package = Package(
                 "Difference",
                 "EngineToolkit"
             ],
-            resources: [
-                .process("TestVectors/"),
-            ]
+            resources: []
         )
     ]
 )
--- Tests/EngineToolkitTests/TXInterfaceTests/InformationTests.swift
+++
@@ -1,9 +0,0 @@
-final class InformationTests: TestCase {
-	
-    func test__information() throws {
-        let information = try sut.information().get()
-        XCTAssertEqual(information, .init(packageVersion: "0.1.0"))
-    }
-}
-
-
