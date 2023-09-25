fd1b31f75c9f7a1e9ff8f93b5de5b537caef1345
Alexander Cyon <alex.cyon@rdx.works>
2023-09-25 16:14:17 +0200

    Lower iOS required version to 11 (UNTESTED), what has been tested is iPhone simulator running iOS 14 on a M1 macOS 13.6 host

--- Tests/EngineToolkitTests/EngineToolkitTests.swift
+++ Tests/EngineToolkitTests/EngineToolkitTests.swift
@@ -1,8 +1,9 @@
-import EngineToolkitUniFFI
+import EngineToolkit
 import XCTest
 
 class EngineToolkitTests: XCTestCase {
-    func test_xz() {
-        assertionFailure()
+    func test_address_from_string() throws {
+        let address = try Address(address: "account_tdx_e_128vkt2fur65p4hqhulfv3h0cknrppwtjsstlttkfamj4jnnpm82gsw")
+        XCTAssertEqual(address.networkId(), 0x0e)
     }
 }
