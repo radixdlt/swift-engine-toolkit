99522126d91ffd9a68479fc5d133c9654c4d67cb
Ghenadie <118184705+GhenadieVP@users.noreply.github.com>
2023-12-08 15:40:27 +0200

    Merge main (#55)
    Co-authored-by: Alexander Cyon <116169792+CyonAlexRDX@users.noreply.github.com>
    Co-authored-by: runner <runner@Mac-1695653571163.local>
    Co-authored-by: runner <runner@Mac-1695994111466.local>
    Co-authored-by: runner <runner@Mac-1698175300221.local>
    Co-authored-by: runner <runner@Mac-1698182096582.local>
    Co-authored-by: Omar <OmarAbdulla7@hotmail.com>
    Co-authored-by: runner <runner@Mac-1698635803864.local>
    Co-authored-by: runner <runner@Mac-1698644206511.local>
    Co-authored-by: runner <runner@Mac-1698752111246.local>
    Co-authored-by: runner <runner@Mac-1698838418683.local>
    Co-authored-by: runner <runner@Mac-1699274493017.local>
    Co-authored-by: runner <runner@Mac-1701085790881.local>
    Co-authored-by: runner <runner@Mac-1701108678850.local>
    Co-authored-by: runner <runner@Mac-1701972605177.local>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import Foundation
@@ -6,7 +6,7 @@ import PackageDescription
 
 let package = Package(
     name: "EngineToolkit",
-    platforms: [.macOS(.v12), .iOS(.v16)],
+    platforms: [.macOS(.v11), .iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
