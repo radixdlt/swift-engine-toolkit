4cf2edc92a430d4f59547d405e4c2c4bc5c912fe
Ghenadie <118184705+GhenadieVP@users.noreply.github.com>
2024-03-10 16:51:49 +0200

    make final classes

--- Sources/EngineToolkit/radix_engine_toolkit_uniffi.swift
+++ Sources/EngineToolkit/radix_engine_toolkit_uniffi.swift
@@ -1018,7 +1018,7 @@ public protocol DecimalProtocol {
     
 }
 
-public class Decimal: DecimalProtocol {
+public final class Decimal: DecimalProtocol {
     fileprivate let pointer: UnsafeMutableRawPointer
 
     // TODO: We'd like this to be `private` but for Swifty reasons,
@@ -3782,7 +3782,7 @@ public protocol NonFungibleGlobalIdProtocol {
     
 }
 
-public class NonFungibleGlobalId: NonFungibleGlobalIdProtocol {
+public final class NonFungibleGlobalId: NonFungibleGlobalIdProtocol {
     fileprivate let pointer: UnsafeMutableRawPointer
 
     // TODO: We'd like this to be `private` but for Swifty reasons,
